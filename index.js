4c58274b572e69ba5695e0d8dd262487a3362e04
Hiroki Daichi <hirokidaichi@gmail.com>
2014-09-18 18:46:45 +0900

    api changed

--- index.js
+++ index.js
@@ -1,112 +1,169 @@
-var Promise = require("promise");
-var util = require("util");
+var promise = (global || window).Promise || require("promise");
+
+var extend = require("extend");
+
+var util = extend(require("util"), require("util-is"));
+var zip = require("zip-object");
+var jsonquery = require("json-query");
 
 var slice = Array.prototype.slice;
-var reserve = module.exports.reserve = function(f) {
-    var reserver = function(d) {
-        return new Promise(function(ok, ng) {
-            f(d, ok, ng)
+
+var k = function(a, b, c) {};
+
+var query = function(data, query) {
+    return jsonquery(query, {
+        data: data
+    }).value;
+};
+
+var transform = function(signature, data) {
+    if (util.isString(signature)) {
+        return query(data, signature);
+    }
+    if (util.isFunction(signature)) {
+        return signature(data);
+    }
+    if (util.isArray(signature)) {
+        return signature.map(function(elem) {
+            return transform(elem, data);
+        });
+    }
+    if (util.isPureObject(signature)) {
+        var keys = Object.keys(signature);
+        return zip(keys, keys.map(function(k) {
+            return transform(signature[k], data);
+        }));
+    }
+};
+
+var flumine = module.exports = function(f) {
+    if (f.isFlumine) return f;
+    if (f.length != 3) return pass.to(f);
+    var fluminer = function() {
+        // 引数の数を0個にし、引数の数で継続を受け取る関数に対応する
+        var d = arguments[0];
+        return new promise(function(ok, ng) {
+            f(d, ok, ng);
         });
     };
-    var _self = reserver;
+    var _self = fluminer;
+    _self.isFlumine = true;
+    Object.keys(flumine.extention).forEach(function(key) {
+        _self[key] = flumine.extention[key].bind(_self);
+    });
 
-    reserver.label = function(reserverName) {
-        return debug(reserverName + " in :")
-            .to(_self)
-            .to(debug(reserverName + " out :"));
-    };
-    reserver.rescue = function(handler) {
-        return reserve(function(d, ok, ng) {
-            _self(d)
-                .
-            catch (handler)
-                .then(ok)
-                .
-            catch (ng);
+    return fluminer;
+};
 
+
+var extention = flumine.extention = {};
+
+extention.rescue = function(handler) {
+    var _self = this;
+    return flumine(function(d, ok, ng) {
+        _self(d)
+        .
+        catch (handler)
+        .then(ok)
+        .
+        catch (ng);
+    });
+};
+
+var fluminize = function(func) {
+    if (func.isFlumine) return func;
+    if (func.length == 3) return flumine(func);
+    return flumine(function(d, ok, ng) {
+        var result = func(d);
+        if (result && result.then) return result.then(ok, ng);
+        if (util.isError(result)) return ng(result);
+        return ok(result);
+    });
+};
+// [request,response = next(request)] の形で返す
+extention.pair = function(n) {
+    var next = fluminize(n);
+    return this.to(function(d, ok, ng) {
+        var req = d;
+        next(d).then(function(res) {
+            ok([req, res]);
+        }).then(ok, ng);
+    });
+};
+
+extention.transform = function(signature) {
+    return this.to(function(d) {
+        return transform(signature, d);
+    });
+};
+
+extention.and = extention.to = function(nextReserver) {
+    var _self = this;
+    var next = fluminize(nextReserver);
+    return flumine(function(d, ok, ng) {
+        _self(d)
+        .then(next)
+        .then(function(d) {
+            ok(d);
         })
-    };
-    reserver.to = function(nextReserver, hasArgs) {
-        var next = (hasArgs != undefined) ? reserve.curry.apply(null, slice.call(arguments)) : nextReserver;
-        return reserve(function(d, ok, ng) {
-            _self(d)
-                .then(next)
-                .then(function(d) {
-                    ok(d)
-                })
-                .
-            catch (ng);
+        .
+        catch (ng);
+    });
+};
+
+extention.first = extention.race = function(next) {
+    var _self = this;
+    if (util.isArray(next)) {
+        return _self.to(function(d) {
+            return promise.race(next.map(function(pf) {
+                return fluminize(pf)(d);
+            }));
         });
-    };
-    reserver.race = function(next) {
-        if (util.isArray(next)) {
-            return _self.to(function(d) {
-                return Promise.race(next.map(function(pf) {
-                    return pf(d);
-                }));
-            });
-        } else {
-            return _self.to(function(d, ok, ng) {
-                return Promise.race((d || []).map(next))
-            });
-        }
-    };
-    reserver.all = function(next) {
-        if (util.isArray(next)) {
-            return _self.to(function(d) {
-                return Promise.all(next.map(function(pf) {
-                    return pf(d);
-                }));
-            });
-        } else {
-            return _self.to(function(d, ok, ng) {
-                return Promise.all((d || []).map(next))
-            });
-        }
-    };
-    return reserver;
+    } else {
+        return _self.to(function(d) {
+            return promise.race((d || []).map(fluminize(next)));
+        });
+    }
+};
+extention.each = extention.all = function(next) {
+    var _self = this;
+    if (util.isArray(next)) {
+        return _self.to(function(d) {
+            return promise.all(next.map(function(pf) {
+                return fluminize(pf)(d);
+            }));
+        });
+    } else {
+        return _self.to(function(d) {
+            return promise.all((d || []).map(fluminize(next)));
+        });
+    }
 };
 
-var pass = reserve(function(d, ok, ng) {
-    ok(d);
-});
-
-reserve.to = function() {
-    return pass.to.apply(null,slice.call(arguments));
-};
-reserve.all = function(reserverList) {
-    return pass.all(reserverList);
-};
-reserve.race = function(reserverList) {
-    return pass.race(reserverList);
-};
-reserve.value = function(value) {
-    return reserve(function(d, ok, ng) {
+extention.fixed = extention.value = function(value) {
+    return this.to(function(d, ok, ng) {
         ok(value);
     });
 };
 
-reserve.curry = function() {
-    var args = slice.call(arguments);
-    var pf = args.shift();
-    return pass.to(function(d) {
-        return pf.apply(null, args);
+extention.set = function(mapper) {
+    var flumineList = Object.keys(mapper).map(function(k) {
+        return fluminize(mapper[k]);
     });
-
-};
-var debug = module.exports.debug = function(message) {
-    return reserve(function(d, ok, ng) {
-        debug.log(message, d);
-        ok(d);
+    return this.each(flumineList).and(function(d) {
+        return zip(Object.keys(mapper), d);
     });
 };
 
-debug.log = console.log;
+var pass = flumine.pass = flumine(function(d, ok, ng) {
+    return ok(d);
+});
 
-var delay = module.exports.delay = function(ms) {
-    return reserve(function(d, ok, ng) {
-        setTimeout(function() {
-            ok(d);
-        }, ms);
-    });
-};
+
+Object.keys(extention).forEach(function(key) {
+    flumine[key] = function() {
+        return pass[key].apply(this, slice.call(arguments));
+    };
+});
+
+
--- test/index.js
+++ test/index.js
@@ -1,99 +1,108 @@
 var flumine = require("../");
+var util = require("../util");
 var assert = require("power-assert");
 var fs = require("fs-promise");
 
 describe("flumine", function() {
-    var reserve = flumine.reserve;
-    var delay = flumine.delay;
-    var debug = flumine.debug;
-
-    describe(".reserve", function() {
-        it("should retern a funtion returning promise", function(done) {
-            var promiseFunc = reserve(function(d, ok, ng) {
-                ok(10);
-            });
-            assert(promiseFunc, "not undefined");
-            assert(promiseFunc(), "not undefined");
-            var p = promiseFunc().then(function(d) {
-                assert(d == 10);
+    var delay = util.delay;
+    var debug = util.debug;
+    it("should retern a funtion returning promise", function(done) {
+        var promiseFunc = flumine(function(d, ok, ng) {
+            ok(10);
+        });
+        assert(promiseFunc, "not undefined");
+        assert(promiseFunc(), "not undefined");
+        var p = promiseFunc().then(function(d) {
+            assert(d == 10);
+        }).then(done, done);
+    });
+    it("should be connectable", function(done) {
+        var double = function(d, ok, ng) {
+            ok(d * 2);
+        };
+        var $double = flumine(double);
+        var $8thtimes = $double.to($double).to(function(d) {
+            return d * 2;
+        });
+        $8thtimes(10).then(function(d) {
+            assert(d == 80);
+        }).then(done, done);
+    });
+    it("should wait all result", function(done) {
+        var delayForValue = flumine(function(d, ok, ng) {
+            var n = Date.now();
+            setTimeout(function(a) {
+                ok(Date.now() - n);
+            }, d);
+        });
+        var forList = flumine.all(delayForValue);
+        forList([100, 200, 300]).then(function(d) {
+            assert(d[0] >= 100);
+            assert(d[1] >= 200);
+            assert(d[2] >= 300);
+        }).then(done, done);
+    });
+    describe("all", function() {
+        it("should wait all fluminers", function(done) {
+            var d1 = flumine.value(100).to(delay(100));
+            var d2 = flumine.value(1000).to(delay(1000));
+            var d3 = flumine.value(500).to(delay(500));
+            var waitAll = flumine.all([d1, d2, d3]);
+            waitAll().then(function(d) {
+                assert.deepEqual(d, [100, 1000, 500]);
             }).then(done, done);
-        });
-        it("should be connectable", function(done) {
-            var double = function(d, ok, ng) {
-                ok(d * 2);
-            };
-            var $double = reserve(double);
-            var $8thtimes = $double.to($double).to(function(d) {
-                return d * 2
-            });
-            $8thtimes(10).then(function(d) {
-                assert(d == 80);
-            }).then(done, done)
-        });
-        it("should wait all result", function(done) {
-            var delayForValue = reserve(function(d, ok, ng) {
-                var n = Date.now()
-                setTimeout(function(a) {
-                    ok(Date.now() - n);
-                }, d);
-            });
-            var forList = reserve.all(delayForValue);
-            forList([100, 200, 300]).then(function(d) {
-                assert(d[0] >= 100);
-                assert(d[1] >= 200);
-                assert(d[2] >= 300);
-            }).then(done, done);
-        });
-        describe("all", function() {
-            it("should wait all reservers", function(done) {
-                var d1 = reserve.value(100).to(delay(100));
-                var d2 = reserve.value(1000).to(delay(1000));
-                var d3 = reserve.value(500).to(delay(500));
-                var waitAll = reserve.all([d1, d2, d3]);
-
-                waitAll().then(function(d) {
-                    assert.deepEqual(d, [100, 1000, 500]);
-                }).then(done, done);
-            })
-        });
-        describe("race", function() {
-            it("should return the fastest reserver", function(done) {
-                var d1 = reserve.value(100).to(delay(100));
-                var d2 = reserve.value(1000).to(delay(1000));
-                var d3 = reserve.value(500).to(delay(500));
-                var waitAll = reserve.race([d1, d2, d3]);
-
-                waitAll().then(function(d) {
-                    assert.equal(d, 100);
-                }).then(done, done);
-            })
-        });
-        describe("curry", function() {
-            it("should make curried function", function(done) {
-                var pf = function(a, b, c, d) {
-                    var Promise = require("Promise");
-                    return new Promise(function(ok, ng) {
-                        ok([a, b, c, d]);
-                    });
-                };
-                var r = reserve.curry(pf, 10, 20, 30, 40);
-                r(11).then(function(d) {
-                    assert.deepEqual(d, [10, 20, 30, 40]);
-                }).then(done, done);
-            });
-            it("should adapt to promise function", function(done) {
-                var existsIndexJs = reserve.to(fs.exists, "./index.js");
-                existsIndexJs().then(function(d) {
-                    assert(d);
-                }).then(done, done);
-            });
-            it("should adapt to promise", function(done) {
-                var existsIndexJs = reserve.value("./inde.js").to(fs.exists);
-                existsIndexJs().then(function(d) {
-                    assert(d == false);
-                }).then(done, done);
-            });
         });
     });
 
-})
+    describe("race", function() {
+        it("should return the fastest fluminer", function(done) {
+            var d1 = flumine.value(100).to(delay(100));
+            var d2 = flumine.value(1000).to(delay(1000));
+            var d3 = flumine.value(500).to(delay(500));
+            var waitAll = flumine.race([d1, d2, d3]);
+
+            waitAll().then(function(d) {
+                assert.equal(d, 100);
+            }).then(done, done);
+        });
+    });
+
+    describe("pair", function() {
+        var add = function(d) {
+            return d + 100;
+        };
+        it("should return result and response pair",
+        flumine.value(10).pair(add).to(function(d) {
+            assert.deepEqual(d, [10, 110]);
+        }));
+
+    });
+    describe("curry", function() {
+        /*
+        it("should make curried function", function(done) {
+            var pf = function(a, b, c, d) {
+                var Promise = require("Promise");
+                return new Promise(function(ok, ng) {
+                    ok([a, b, c, d]);
+                });
+            };
+            var r = flumine.curry(pf, 10, 20, 30, 40);
+            r(11).then(function(d) {
+                assert.deepEqual(d, [10, 20, 30, 40]);
+            }).then(done, done);
+        });
+        it("should adapt to promise function", function(done) {
+            var existsIndexJs = flumine.to(fs.exists, "./index.js");
+            existsIndexJs().then(function(d) {
+                assert(d);
+            }).then(done, done);
+        });
+        it("should adapt to promise", function(done) {
+            var existsIndexJs = flumine.value("./inde.js").to(fs.exists);
+            existsIndexJs().then(function(d) {
+                assert(d === false);
+            }).then(done, done);
+        });
+        */
+    });
+});
